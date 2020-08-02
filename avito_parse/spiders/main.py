ba5426d01c4da930dd4c3ad158823aea3d17fdc2
RifaatMZ <muhannadzr@me.com>
2020-08-02 19:55:42 +0300

    homework_5

--- avito_parse/spiders/main.py
+++ avito_parse/spiders/main.py
@@ -4,11 +4,17 @@ from scrapy.settings import Settings
 from avito_parse import settings
 from avito_parse.spiders.avito import AvitoSpider
 
+from avito_parse.spiders.habr import HabrSpider
+
 if __name__ == '__main__':
     crawl_settigns = Settings()
     crawl_settigns.setmodule(settings)
     crawol_proc = CrawlerProcess(settings=crawl_settigns)
 
-    crawol_proc.crawl(AvitoSpider)
+    # crawol_proc.crawl(AvitoSpider)
+    crawol_proc.crawl(HabrSpider)
 
     crawol_proc.start()
+
+
+
